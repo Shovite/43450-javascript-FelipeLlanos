e9b64c614b193d43375ab4e449b833922662cc0d
FelipeLlanos <him_tata@hotmail.com>
2023-06-17 14:41:26 -0400

    version con ajustes

--- scripts.js
+++ scripts.js
@@ -2,32 +2,30 @@ function nextId(){
     let newid = productos.length + 1
 return newid
 }
-
 function crearProducto(){
     const id = nextId()
-    const nombre = prompt("Ingrese nombre producto")
-    const precio = prompt("Ingrese precio") // hay que hacerlo numero
-    const stock = prompt("Ingrese Stock") // hay que hacerlo numero
+    const nombre = prompt("Ingrese nombre producto").toLowerCase()
+    const precio = parseInt(prompt("Ingrese precio"))
+    const stock = parseInt(prompt("Ingrese Stock"))
     const nuevoProducto = new Producto(id, nombre, precio, stock)
     productos.push(nuevoProducto)
+    alert(nombre + " agregado/a")
 } 
-
 function venderProducto(){
-    let nombreProducto = prompt("¿que productos quieres vender?")
+    let nombreProducto = prompt("¿que productos quieres vender?").toLowerCase()
     const venta = productos.find((producto)=> producto.nombre == nombreProducto)
     if(venta === undefined){
-        console.warn("Producto no encontrado")
+        alert("Producto no encontrado")
     } else {
         if(venta.stock <= 0){
-            console.warn("No queda stock de " + venta.nombre)
+            alert("No queda stock de " + venta.nombre)
         } else {
             venta.stock = venta.stock - 1
-            console.log("te quedan " + venta.stock + " " + venta.nombre)
+            alert("¡VENDIDO!")
+            alert("te queda(n) " + venta.stock + " " + venta.nombre + " disponible(s)")
         }
-        
     }
 }
-
 function menorPrecio(){
     const productosOrdenados = productos
     productosOrdenados.sort((a,b) => {
@@ -39,9 +37,39 @@ function menorPrecio(){
         }
         return 0
     })
-    console.log(productosOrdenados)
+    alert("Estos son los productos actuales de menor a mayor precio:")
+    for(let i = 0; i < productosOrdenados.length; i++){
+        
+        alert(productosOrdenados[i].nombre + " || $" + productosOrdenados[i].precio + " || " + productosOrdenados[i].stock + " unidades")
+    }
 }
-
-
-console.log(venderProducto())
-console.log(productos)
+function start(){
+    let nombreVendedor = prompt("Favor ingresa tu nombre de vendedor")
+    let validadorNumerico = isNaN((nombreVendedor))
+    while (nombreVendedor == null || validadorNumerico == false){
+        nombreVendedor = prompt("Nombre no valido, favor ingresar nuevamente")
+        validadorNumerico = isNaN((nombreVendedor))
+    }
+    alert("¡Hola " + nombreVendedor + "!")
+    let accion
+    while(accion != "salir"){
+        accion = prompt("Indica que accion necesitas realizar: 'VENDER', 'CREAR PRODUCTO', 'CONSULTAR' o 'SALIR'").toLowerCase()
+        switch(accion){
+            case "crear producto":
+                crearProducto()
+                break;
+            case "vender":
+                venderProducto()
+                break;
+            case "consultar":
+                menorPrecio()
+                break;
+            case "salir":
+                alert("¡Adios " + nombreVendedor + "!")
+                break;
+            default:
+                alert("¡OPCION NO VALIDA!")
+                break;
+        }   
+    }
+}
--- variables.js
+++ variables.js
@@ -7,6 +7,6 @@ class Producto {
     }
 }
 const carrito = []
-const productos =  [{id: 1, nombre: 'Becker', precio: 2500, stock: 2},
-                    {id: 2, nombre: 'Escudo', precio: 3500, stock: 1},
-                    {id: 3, nombre: 'Cristal', precio: 2000, stock: 2}]
+const productos =  [{id: 1, nombre: 'becker', precio: 2500, stock: 2},
+                    {id: 2, nombre: 'escudo', precio: 3500, stock: 1},
+                    {id: 3, nombre: 'cristal', precio: 2000, stock: 2}]
